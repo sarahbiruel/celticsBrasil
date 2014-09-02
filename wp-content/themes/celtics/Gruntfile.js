2989b1fe57bda50c7a95da89905ec2f372698973
Diego Incerti <diego_incerti@hotmail.com>
2014-09-02 17:50:40 -0300

    Grunt tasks configuradas

--- wp-content/themes/celtics/Gruntfile.js
+++ wp-content/themes/celtics/Gruntfile.js
@@ -1,118 +1,90 @@
-module.exports = function( grunt ) {
+module.exports = function(grunt) {
 	'use strict';
 
 	// Load all grunt tasks
 	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
 	// Project configuration
-	grunt.initConfig( {
-		pkg:    grunt.file.readJSON( 'package.json' ),
-		concat: {
-			options: {
-				stripBanners: true,
-				banner: '/*! <%= pkg.title %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
-					' * <%= pkg.homepage %>\n' +
-					' * Copyright (c) <%= grunt.template.today("yyyy") %>;' +
-					' * Licensed GPLv2+' +
-					' */\n'
-			},
-			celtics_brasil: {
-				src: [
-					'assets/js/src/celtics_brasil.js'
-				],
-				dest: 'assets/js/celtics_brasil.js'
+	grunt.initConfig({
+		pkg : grunt.file.readJSON('package.json'),
+		concat : {
+			celtics_brasil : {
+				src : ['bootstrap/assets/js/transition.js', 'bootstrap/assets/js/button.js', 'bootstrap/assets/js/collapse.js', 'bootstrap/assets/js/dropdown.js', 'bootstrap/assets/js/tooltip.js', 'assets/js/src/**/*.js'],
+				dest : 'assets/js/celtics_brasil.js'
 			}
 		},
-		jshint: {
-			browser: {
-				all: [
-					'assets/js/src/**/*.js',
-					'assets/js/test/**/*.js'
-				],
-				options: {
-					jshintrc: '.jshintrc'
+		jshint : {
+			browser : {
+				all : ['assets/js/src/**/*.js'],
+				options : {
+					jshintrc : '.jshintrc'
 				}
 			},
-			grunt: {
-				all: [
-					'Gruntfile.js'
-				],
-				options: {
-					jshintrc: '.gruntjshintrc'
+			grunt : {
+				all : ['Gruntfile.js'],
+				options : {
+					jshintrc : '.gruntjshintrc'
 				}
-			}   
+			}
 		},
-		uglify: {
-			all: {
-				files: {
-					'assets/js/celtics_brasil.min.js': ['assets/js/celtics_brasil.js']
+		uglify : {
+			all : {
+				files : {
+					'assets/js/celtics_brasil.min.js' : ['assets/js/celtics_brasil.js']
 				},
-				options: {
-					banner: '/*! <%= pkg.title %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
-						' * <%= pkg.homepage %>\n' +
-						' * Copyright (c) <%= grunt.template.today("yyyy") %>;' +
-						' * Licensed GPLv2+' +
-						' */\n',
-					mangle: {
-						except: ['jQuery']
+				options : {
+					banner : '/*! <%= pkg.title %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' + ' * <%= pkg.homepage %>\n' + ' * Copyright (c) <%= grunt.template.today("yyyy") %>;' + ' * Licensed GPLv2+' + ' */\n',
+					mangle : {
+						except : ['jQuery']
 					}
 				}
 			}
 		},
-		test:   {
-			files: ['assets/js/test/**/*.js']
-		},
-		
-		sass:   {
-			all: {
-				files: {
-					'assets/css/celtics_brasil.css': 'assets/css/sass/celtics_brasil.scss'
+		sass : {
+			all : {
+				files : {
+					'assets/css/celtics_brasil.css' : 'assets/css/sass/celtics_brasil.scss',
+					'bootstrap/assets/css/bootstrap.css' : 'bootstrap/assets/css/sass/bootstrap.scss'
 				}
 			}
 		},
-		
-		cssmin: {
-			options: {
-				banner: '/*! <%= pkg.title %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
-					' * <%= pkg.homepage %>\n' +
-					' * Copyright (c) <%= grunt.template.today("yyyy") %>;' +
-					' * Licensed GPLv2+' +
-					' */\n'
+
+		cssmin : {
+			options : {
+				banner : '/*! <%= pkg.title %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' + ' * <%= pkg.homepage %>\n' + ' * Copyright (c) <%= grunt.template.today("yyyy") %>;' + ' * Licensed GPLv2+' + ' */\n'
 			},
-			minify: {
-				expand: true,
-				
-				cwd: 'assets/css/',
-				src: ['celtics_brasil.css'],
-				
-				dest: 'assets/css/',
-				ext: '.min.css'
+			minify : {
+				expand : true,
+
+				cwd : 'assets/css/',
+				src : 'celtics_brasil.css',
+
+				dest : 'assets/css/',
+				ext : '.min.css'
 			}
 		},
-		watch:  {
-			
-			sass: {
-				files: ['assets/css/sass/*.scss'],
-				tasks: ['sass', 'cssmin'],
-				options: {
-					debounceDelay: 500
-				}
+		watch : {
+			sass : {
+				files : ['assets/css/sass/**/*.scss', 'bootstrap/assets/css/sass/**/*.scss'],
+				tasks : ['sass', 'cssmin'],
+				options : {
+					livereload : true,
+				},
 			},
-			
-			scripts: {
-				files: ['assets/js/src/**/*.js', 'assets/js/vendor/**/*.js'],
-				tasks: ['jshint', 'concat', 'uglify'],
-				options: {
-					debounceDelay: 500
-				}
+
+			scripts : {
+				files : ['assets/js/src/**/*.js'],
+				tasks : ['jshint', 'concat', 'uglify'],
+				options : {
+					livereload : true,
+				},
 			}
 		}
-	} );
+	});
 
 	// Default task.
-	
-	grunt.registerTask( 'default', ['jshint', 'concat', 'uglify', 'sass', 'cssmin'] );
-	
+
+	grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'sass', 'cssmin']);
 
 	grunt.util.linefeed = '\n';
-};
+}; 
